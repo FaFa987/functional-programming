1b53a40409dcaa584426da7402d5c3f0d686399c
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-25 23:23:16 +0100

    main -> Anonymous Inner Class

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,8 +1,22 @@
 package se.lexicon;
 
+import se.lexicon.interfaces.DoStringStuff;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+
+    //Anonymous Inner Class
+    static DoStringStuff concatString = new DoStringStuff() {
+
+        @Override
+        public String oparate(String s1, String s2) {
+            return s1 +" AND "+ s2;
+        }
+    };
+
+
     public static void main(String[] args) {
 
         StringConcatenate concat = new StringConcatenate();
@@ -10,6 +24,8 @@ public class Main {
 
         StringBiggest stringBiggest = new StringBiggest();
         System.out.println(stringBiggest.oparate("Jill", "Hamond"));
+
+        System.out.println(concatString.oparate("Mohammad" , "Hossein"));
 
 
     }

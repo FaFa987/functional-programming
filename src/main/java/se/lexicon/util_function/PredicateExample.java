ee0ac3bc8656ca90e299031b9ebcbbf39a5e5993
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-29 20:19:34 +0100

    PredicateExample isPositive.and(isOdd) - isPositive.negate()

--- src/main/java/se/lexicon/util_function/PredicateExample.java
+++ src/main/java/se/lexicon/util_function/PredicateExample.java
@@ -18,10 +18,25 @@ public class PredicateExample {
         Predicate<Integer> isPositive = i -> i > 0;
         Predicate<Integer> isOdd = i -> i % 2 != 0;
 
-        for (Integer i : number) {
+        /*for (Integer i : number) {
             if (isPositive.test(i)) {
                 System.out.println(i);
             }
+        }*/
+        System.out.println(filterNumbers(number, isPositive));
+        System.out.println(filterNumbers(number, isPositive.and(isOdd)));
+        System.out.println(filterNumbers(number, isPositive.negate()));
+
+    }
+
+    public static List<Integer> filterNumbers(List<Integer> numbers, Predicate<Integer> predicate) {
+        List<Integer> filteredNumbers = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            if (predicate.test(number)) {
+                filteredNumbers.add(number);
+            }
         }
+        return filteredNumbers;
     }
 }

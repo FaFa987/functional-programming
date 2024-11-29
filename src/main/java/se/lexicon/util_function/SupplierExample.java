1ad5d844fd43a1c4431990e2772e3de35698f105
Fatemeh <f.khalleghi.777@gmail.com>
2024-11-29 23:10:21 +0100

    while (true) { try/catch}

--- src/main/java/se/lexicon/util_function/SupplierExample.java
+++ src/main/java/se/lexicon/util_function/SupplierExample.java
@@ -1,9 +1,6 @@
 package se.lexicon.util_function;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Supplier;
 
 public class SupplierExample {
@@ -20,6 +17,26 @@ public class SupplierExample {
 
         System.out.println(init.get());
         System.out.println(sessionGenarator.get());
+
+        System.out.println(takeDecimalInput.get());
     }
+
+    public static Supplier<Double> takeDecimalInput = () -> {
+
+        Double number;
+        while (true) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter a number: ");
+                number = sc.nextDouble();
+                break;
+            }catch (InputMismatchException e) {
+                System.out.println("Invalid input");
+            }
+
+        }
+        return number;
+
+    };
 
 }
